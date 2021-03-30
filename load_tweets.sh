3ce170231f8ba666247c532a868f85e9acf2ba9d
Eamnuel Jarquin <ejarquin22@cmc.edu>
2021-03-29 18:28:13 -0700

    Initial Commit

--- load_tweets.sh
+++ load_tweets.sh
@@ -4,8 +4,10 @@ test-data.zip
 
 for file in $files; do
     # call the load_tweets.py file to load data into pg_normalized
+    python3 load_tweets.py --db postgresql://postgres:pass@localhost:16969/postgres --inputs test-data.zip
 done
 
 for file in $files; do
     # use SQL's COPY command to load data into pg_denormalized
+    unzip -p test-data.zip | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:16939/postgres -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
